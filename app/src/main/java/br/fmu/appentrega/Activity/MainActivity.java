802142c4d4d1bdc1c0bca51bc8413ff7179027fd
gabriel kupstaite <gabrielkupstaite@gmail.com>
2022-11-20 18:37:35 -0300

    Novas imagens e configurações do main

--- app/src/main/java/br/fmu/appentrega/Activity/MainActivity.java
+++ app/src/main/java/br/fmu/appentrega/Activity/MainActivity.java
@@ -1,16 +1,27 @@
 package br.fmu.appentrega.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import br.fmu.appentrega.R;
 
 public class MainActivity extends AppCompatActivity {
-
+private RecyclerView.Adapter adapter;
+private RecyclerView recyclerViewCategoryList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory(){
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList=findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
     }
 }
