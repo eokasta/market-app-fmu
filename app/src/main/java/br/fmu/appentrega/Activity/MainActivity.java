da8893267137aaf0790212feb4cda8da5e8434a6
gabriel kupstaite <gabrielkupstaite@gmail.com>
2022-11-20 21:44:53 -0300

    Segundo recycler com array de comidas

--- app/src/main/java/br/fmu/appentrega/Activity/MainActivity.java
+++ app/src/main/java/br/fmu/appentrega/Activity/MainActivity.java
@@ -10,11 +10,12 @@ import java.util.ArrayList;
 
 import br.fmu.appentrega.Adaptador.AdaptadorCategoria;
 import br.fmu.appentrega.Domain.CategoryDomain;
+import br.fmu.appentrega.Domain.ComidaDomain;
 import br.fmu.appentrega.R;
 
 public class MainActivity extends AppCompatActivity {
-private RecyclerView.Adapter adapter;
-private RecyclerView recyclerViewCategoryList;
+private RecyclerView.Adapter adapter, adapter2;
+private RecyclerView recyclerViewCategoryList, recyclerViewPopularList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,5 +38,14 @@ private RecyclerView recyclerViewCategoryList;
 
         adapter = new AdaptadorCategoria(category);
         recyclerViewCategoryList.setAdapter(adapter);
+    }
+    private void recyclerViewPopular(){
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewPopularList = findViewById(R.id.recyclerView2);
+
+        ArrayList<ComidaDomain> comidaList=new ArrayList<>();
+        comidaList.add(new ComidaDomain("Pizza de Pepperoni", "pizza1", "fatias de pepperoni, muçarela, orégano, molho de tomate", 9.76));
+        comidaList.add(new ComidaDomain("X Burguer", "burguer", "Carne, queijo Gouda, Molho especial, alface, tomate", 8.79));
+        comidaList.add(new ComidaDomain("Pizza vegetáriana", "pizza2","Óleo de oliva", 10.13));
     }
 }
